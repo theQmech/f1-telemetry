b5f82332a762d572c7df729a904fd8a5cb674476
Rupanshu Ganvir <rupanshu.ganvir@gmail.com>
2023-10-15 14:53:15 +0200

    Add packets source option to mock udp feed script

--- docker/generate_udp_feed.py
+++ docker/generate_udp_feed.py
@@ -5,12 +5,13 @@ import time
 sink_host = str(os.environ["SINK_HOST"])
 sink_port = int(os.environ["SINK_PORT"])
 wait_ms = float(os.environ["WAIT_MS"])
+packets_src = str(os.environ["PACKETS_SRC"])
 
 for i in range(14_000):
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  fname = f"/packet_dump/{i}.bin"
-  with open(fname, 'rb') as f:
-    raw_data = f.read()
-    sock.sendto(raw_data, (sink_host, sink_port))
-    print(f"Sent file {fname} {len(raw_data)}B to {sink_host}:{sink_port}")
-    time.sleep(wait_ms / 1000)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    fname = f"{packets_src}/{i}.bin"
+    with open(fname, "rb") as f:
+        raw_data = f.read()
+        sock.sendto(raw_data, (sink_host, sink_port))
+        print(f"Sent file {fname} {len(raw_data)}B to {sink_host}:{sink_port}")
+        time.sleep(wait_ms / 1000)
